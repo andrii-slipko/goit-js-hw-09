236409dc33bbd1dddf3477e9de8c885e6e27a4ae
andrii-slipko <andreyslipko0703@gmail.com>
2024-09-05 14:42:42 +0300

    hw9

---
+++ src/js/2-form.js
@@ -0,0 +1,55 @@
+const formData = {
+    email: "",
+    message: ""
+};
+
+
+const form = document.querySelector('.feedback-form');
+const emailInput = document.querySelector('.email');
+const messageTextarea = document.querySelector('.message');
+
+
+function saveToLocalStorage() {
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
+function loadFromLocalStorage() {
+    const savedData = localStorage.getItem('feedback-form-state');
+    if (savedData) {
+        const parsedData = JSON.parse(savedData);
+        formData.email = parsedData.email;
+        formData.message = parsedData.message;
+
+        emailInput.value = formData.email;
+        messageTextarea.value = formData.message;
+    }
+}
+
+
+loadFromLocalStorage();
+
+
+form.addEventListener('input', (event) => {
+    if (event.target.name === 'email') {
+        formData.email = event.target.value;
+    } else if (event.target.name === 'message') {
+        formData.message = event.target.value;
+    }
+    saveToLocalStorage();
+});
+
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    if (formData.email === '' || formData.message === '') {
+        alert('Fill please all fields');
+    } else {
+        console.log(formData);
+        localStorage.removeItem('feedback-form-state');
+        formData.email = '';
+        formData.message = '';
+        emailInput.value = '';
+        messageTextarea.value = '';
+    }
+});
