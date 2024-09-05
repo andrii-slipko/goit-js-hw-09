50764c4b06b1d21cbeb1533f14247ed5eb2bd3d3
andrii-slipko <andreyslipko0703@gmail.com>
2024-09-06 01:01:11 +0300

    hw9 trim

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -41,7 +41,8 @@ form.addEventListener('input', (event) => {
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
-
+    formData.email = formData.email.trim();
+    formData.message = formData.message.trim();
     if (formData.email === '' || formData.message === '') {
         alert('Fill please all fields');
     } else {
